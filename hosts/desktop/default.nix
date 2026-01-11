77e8903e07118672d0854135c993a060708b8209
HANDZCZ <handz@email.cz>
2026-01-11 15:58:48 +0100

    Add LC_* locale settings

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -21,6 +21,20 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ALL		= "en_US.UTF-8";
+      LC_CTYPE		= "cs_CZ.UTF-8";
+      LC_ADDRESS	= "cs_CZ.UTF-8";
+      LC_MEASUREMENT	= "cs_CZ.UTF-8";
+      LC_MESSAGES	= "en_US.UTF-8";
+      LC_MONETARY	= "cs_CZ.UTF-8";
+      LC_NAME		= "cs_CZ.UTF-8";
+      LC_NUMERIC	= "cs_CZ.UTF-8";
+      LC_PAPER		= "cs_CZ.UTF-8";
+      LC_TELEPHONE	= "cs_CZ.UTF-8";
+      LC_TIME		= "cs_CZ.UTF-8";
+      LC_COLLATE	= "cs_CZ.UTF-8";
+    };
   };
 
   console = {
