6986d15803067b3fe7c2bd3b024351e4965e36e2
HANDZCZ <handz@email.cz>
2026-01-15 16:58:14 +0100

    Enable zramSwap
    Should use zswap instead, will see if it is enough or if I need to add swap on disk and switch to zswap

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -16,7 +16,23 @@
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3;
   boot.kernelParams = [
     "video=DP-2:2560x1440@180"
+    "zswap.enabled=0"
   ];
+
+  boot.kernel.sysctl = {
+    "vm.vfs_cache_pressure" = 300;      # Pop!_OS guides: 200-400 frees caches fast for zram, but 300 balances ZFS reads.
+    "vm.swappiness" = 180;              # SteamOS/Bazzite: 150-180 treats zram as "RAM extension"—snappy on low loads.
+    "vm.dirty_background_ratio" = 20;   # Gaming tunings: 1-5 prevents micro-lags from bursts; 2 suits light VMs.            # 20
+    "vm.dirty_ratio" = 60;              # Balanced gamer mid-range—allows dirty buildup without app stalls.                  # 80
+    "vm.watermark_boost_factor" = 0;    # Disables overhead (all zram sources agree).
+    "vm.watermark_scale_factor" = 125;  # Proactive headroom—gamer/Proxmox default for no surprises.
+    "vm.page-cluster" = 0;
+  };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+  };
 
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
