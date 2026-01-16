b476e6bb40e11764327ec7b5faa78fd524487d1c
HANDZCZ <handz@email.cz>
2026-01-16 22:17:30 +0100

    Add opacity to alacritty

--- users/handz/alacritty.nix
+++ users/handz/alacritty.nix
@@ -4,7 +4,10 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      window.padding = { x = 5; y = 5; };
+      window = {
+        padding = { x = 5; y = 5; };
+        opacity = 0.8;
+      };
     };
   };
 }
