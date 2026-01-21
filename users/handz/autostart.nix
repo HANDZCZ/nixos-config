ace063f02b242421736a68fc4630dc1e65a81e3c
HANDZCZ <handz@email.cz>
2026-01-21 14:27:12 +0100

    Edit autostart - add more helper functions

--- users/handz/autostart.nix
+++ users/handz/autostart.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, pkgs-unstable, ... }:
 
 let
   nixcord = config.programs.nixcord;
@@ -11,12 +11,14 @@ let
       destination = "/";
     } + "/${name}-autostart.desktop";
   mkCondEntry = condition: name: command: lib.mkIf condition (mkEntry name command);
+  isInPackages = package: lib.elem pkgs.${package} config.home.packages || lib.elem pkgs-unstable.${package} config.home.packages;
+  mkInPkgsEntry = package: name: command: mkCondEntry (isInPackages package) name command;
 in {
   xdg.autostart = {
     enable = true;
     entries = [
       (mkCondEntry (nixcord.enable && nixcord.discord.enable) "Discord" "discord --start-minimized")
-      (mkCondEntry (lib.elem pkgs.qbittorrent config.home.packages) "qBittorrent" "qbittorrent --confirm-legal-notice")
+      (mkInPkgsEntry "qbittorrent" "qBittorrent" "qbittorrent --confirm-legal-notice")
     ];
   };
 }
