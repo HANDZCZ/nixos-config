154d42c3a430fc074f0d5306f9524b084f08b9f5
HANDZCZ <handz@email.cz>
2026-01-23 22:00:49 +0100

    Edit autostart helper fn
    Make isInPackages also check systemPackages

--- users/handz/autostart.nix
+++ users/handz/autostart.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, pkgs-unstable, ... }:
+{ config, osConfig, lib, pkgs, pkgs-unstable, ... }:
 
 let
   nixcord = config.programs.nixcord;
@@ -11,7 +11,11 @@ let
       destination = "/";
     } + "/${name}-autostart.desktop";
   mkCondEntry = condition: name: command: lib.mkIf condition (mkEntry name command);
-  isInPackages = package: lib.elem pkgs.${package} config.home.packages || lib.elem pkgs-unstable.${package} config.home.packages;
+  isInPackages = package:
+    lib.elem pkgs.${package} config.home.packages
+    || lib.elem pkgs-unstable.${package} config.home.packages
+    || lib.elem pkgs.${package} osConfig.environment.systemPackages
+    || lib.elem pkgs-unstable.${package} osConfig.environment.systemPackages;
   mkInPkgsEntry = package: name: command: mkCondEntry (isInPackages package) name command;
 in {
   xdg.autostart = {
