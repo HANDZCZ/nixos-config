013c9b4b9473d412c89d484855a85711c22eb7ca
HANDZCZ <handz@email.cz>
2026-01-31 22:17:49 +0100

    Fix grub being slow on high resolution monitor

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -36,10 +36,15 @@ in {
     algorithm = "zstd";
   };
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = false;
+    gfxmodeEfi = "1024x768";
+    memtest86.enable = true;
+    configurationLimit = 50;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-desktop";
