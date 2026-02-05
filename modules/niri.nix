1ae094fc84510082cb06b97fe4d070579ec3dfaf
HANDZCZ <handz@email.cz>
2026-02-06 00:16:44 +0100

    Switch xwayland-satellite to stable
    0.8.0 version is in stable now

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,14 +1,9 @@
-{ pkgs, pkgs-unstable, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
-    # TODO: change back to stable once version 0.8.0 hits stable
-    # fixes: steam crashing on startup, and bringing down with it all other apps dependent on xwayland-sattelite
-    #        https://github.com/Supreeeme/xwayland-satellite/issues/282
-    #        https://discourse.nixos.org/t/steam-not-launching-in-25-11/73536
-    pkgs-unstable.xwayland-satellite
+    xwayland-satellite
     alacritty
-    swayidle
     wev
   ];
 
