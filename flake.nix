1d0daba9db0b1794f7741e3c3058661c2b9fa4da
HANDZCZ <handz@email.cz>
2026-03-02 20:52:59 +0100

    Update noctalia to v4.6.1

--- flake.nix
+++ flake.nix
@@ -16,9 +16,16 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    noctalia-qs = {
+      url = "github:noctalia-dev/noctalia-qs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        noctalia-qs.follows = "noctalia-qs";
+      };
     };
 
     nixcord = {
