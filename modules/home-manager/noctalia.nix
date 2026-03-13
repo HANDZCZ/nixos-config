1178eb8f83fcc08b1d2ad69ccd5e4c9e926f8878
HANDZCZ <handz@email.cz>
2026-03-13 18:37:45 +0100

    Edit noctalia module
    Adds options for extra packages and overlays for package
    Removes the unnecessary packages in home.packages and moves app2unit to extraPackages option

--- modules/home-manager/noctalia.nix
+++ modules/home-manager/noctalia.nix
@@ -1,18 +1,65 @@
 { pkgs, lib, config, inputs, ... }:
 
-{
+let
+  cfg = config.programs.noctalia-shell;
+  overlayType = lib.mkOptionType {
+    name = "nixpkgs-overlay";
+    description = "nixpkgs overlay";
+    check = lib.isFunction;
+    merge = lib.mergeOneOption;
+  };
+in {
   imports = [
     inputs.noctalia.homeModules.default
   ];
 
-  programs.noctalia-shell = {
-    enable = true;
-    systemd.enable = true;
-    package = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  options.programs.noctalia-shell = {
+    basePackage = lib.mkOption {
+      type = lib.types.package;
+      default = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      description = "Base noctalia-shell package to which overlays will be applied.";
+    };
+    overlays = lib.mkOption {
+      type = lib.types.listOf overlayType;
+      default = [];
+      description = ''
+        List of overlays that override the package in some way
+        and return the overriden package.
+      '';
+      example = lib.literalExpression ''
+        [
+          (final: prev: prev.override {
+            extraPackages = [ pkgs.python3 ];
+          })
+        ]
+      '';
+    };
+    extraPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [];
+      description = ''
+        List of addition packages that will be made available to noctalia.
+      '';
+      example = lib.literalExpression ''
+        with pkgs; [
+          bash
+          gnused
+        ]
+      '';
+    };
   };
 
-  home.packages = with pkgs; [
-    cliphist
-    xdg-desktop-portal
-  ] ++ lib.optionals config.programs.noctalia-shell.settings.appLauncher.useApp2Unit [ app2unit ];
+  config.programs.noctalia-shell = let
+    merged_overlays = lib.composeManyExtensions cfg.overlays;
+    custom_package = lib.fix (lib.extends merged_overlays (_: cfg.basePackage));
+    package = if cfg.overlays == [] then cfg.basePackage else custom_package;
+
+    final_package = package.override (old-noct: {
+      extraPackages = (old-noct.extraPackages or []) ++ cfg.extraPackages;
+    });
+  in {
+    systemd.enable = lib.mkDefault true;
+    package = lib.mkForce final_package;
+    extraPackages = lib.optional (lib.attrByPath [ "appLauncher" "useApp2Unit" ] false cfg.settings) pkgs.app2unit;
+  };
 }
