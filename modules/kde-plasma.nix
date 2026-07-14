3bdf04e299fbc215347b6e2d3c9a85c72c9ab637
HANDZCZ <handz@email.cz>
2026-07-14 19:32:59 +0200

    Add kde-plasma module

---
+++ modules/kde-plasma.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  services = {
+    desktopManager.plasma6.enable = true;
+    displayManager.plasma-login-manager.enable = true;
+  };
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+  ];
+}
