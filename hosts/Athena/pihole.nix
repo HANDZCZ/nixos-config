0d57558138eff4d460ce1048c6f106dca17915a3
HANDZCZ <handz@email.cz>
2026-08-27 13:00:11 +0200

    Add dnscrypt-proxy to Athena
    Use dnscrypt-proxy as upstream for pihole

--- hosts/Athena/pihole.nix
+++ hosts/Athena/pihole.nix
@@ -3,18 +3,29 @@
 {
   imports = [
     ../../modules/pihole.nix
+    ../../modules/dnscrypt-proxy.nix
   ];
 
   services.pihole-ftl = {
     settings = {
       #misc.readOnly = false;
       dns = {
-        upstreams = [ "1.1.1.1" ];
+        upstreams = [ "127.0.0.1#1053" ];
         revServers = [];
         hosts = [];
         cnameRecords = [];
         #interface = "eth0";
       };
+    };
+  };
+
+  services.dnscrypt-proxy = {
+    listenOn = [ "127.0.0.1:1053" ];
+    ipv6Support = false;
+    useCache = false; # handled by pihole
+    webui = {
+      enable = true;
+      openFirewall = true;
     };
   };
 }
