4e6372ae8d7239b9b80d9f7b3791aeaa07f52949
HANDZCZ <handz@email.cz>
2026-08-30 21:17:47 +0200

    Add warnings for ports allowed on all interfaces in Athena

--- hosts/Athena/default.nix
+++ hosts/Athena/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
@@ -10,6 +10,14 @@
   ];
 
   services.openssh.enable = true;
+
+  warnings = let
+    fw-cfg = config.networking.firewall;
+    mkPortWarn = proto: port: "${proto} ${toString port} is allowed on all interfaces!";
+  in lib.map (port: mkPortWarn "TCP" port) fw-cfg.allowedTCPPorts
+    ++ lib.map (port: mkPortWarn "TCP" port) fw-cfg.allowedTCPPortRanges
+    ++ lib.map (port: mkPortWarn "UDP" port) fw-cfg.allowedUDPPorts
+    ++ lib.map (port: mkPortWarn "UDP" port) fw-cfg.allowedUDPPortRanges;
 
   nix.settings = {
     trusted-users = [ "handz" ];
