b667b6029e2c4e3a5acd27dd442cfde7d54ebef1
HANDZCZ <handz@email.cz>
2026-08-31 19:04:56 +0200

    Add PBR and use symmetric routing for vlans in Athena
    Fixes misbehaving routing

--- hosts/Athena/network.nix
+++ hosts/Athena/network.nix
@@ -3,6 +3,8 @@
 let
   net-cfg = config.systemd.network;
   required-networks = lib.attrValues networks;
+
+  vlan-offset = 15000;
 in {
   warnings = required-networks
     |> lib.filter (name: !net-cfg.netdevs ? "10-${name}")
@@ -55,15 +57,41 @@ in {
     ];
 
     networks = let
-      mkConfVlan = name: {
+      mkConfVlan = name: let
+        vlan-id = net-cfg.netdevs."10-vlan-${name}".vlanConfig.Id;
+        vlan-offset-id = vlan-id + vlan-offset;
+      in {
         "30-vlan-${name}" = {
           matchConfig.Name = "vlan-${name}";
           networkConfig.DHCP = "ipv4";
           dhcpV4Config = {
             UseDNS = false;
-            UseRoutes = false;
             UseNTP = false;
           };
+          # We need to handle cross subnet traffic
+          # otherwise egress will want to go through default gateway and get blocked by rp_filter (from firewall)
+          # so we will use PBR and isolate vlan routes to specific table
+          # and turn it into symmetric routing
+          dhcpV4Config = {
+            RouteTable = vlan-offset-id;
+            RouteMetric = 1024;
+          };
+          # Add vlan route to main table
+          # so server-generated traffic can still be sent through the right vlan
+          # and not through default gateway only
+          routes = [{
+            Gateway = "_dhcp4";
+            Table = "main";
+            Metric = 1024;
+          }];
+          # Add PBR rule to route traffic through vlan table
+          # if it has vlan firewall mark
+          routingPolicyRules = [
+            {
+              FirewallMark = vlan-offset-id;
+              Table = vlan-offset-id;
+            }
+          ];
         };
       };
     in lib.mkMerge [
@@ -92,14 +120,59 @@ in {
       }
 
       # Configure devices
-      {
+      /*{
         "30-vlan-servers" = {
           matchConfig.Name = "vlan-servers";
           networkConfig.DHCP = "ipv4";
+          #dhcpV4Config.RouteMetric = 512;
         };
+      }*/
+      (mkConfVlan "servers")
+      {
+        "30-vlan-servers".routes = [{
+          Gateway = "_dhcp4";
+          Destination = "0.0.0.0/0";
+          Table = "main";
+          Metric = 512;
+        }];
       }
       (mkConfVlan "lan")
       (mkConfVlan "iot-net")
     ];
+  };
+
+  networking.nftables = let
+    # Marks incoming traffic with vlan specific mark if the traffic belongs to it
+    mkVlanMark = name:
+      ''ct mark 0 iifname "vlan-${name}" ct mark set ${toString (net-cfg.netdevs."10-vlan-${name}".vlanConfig.Id + vlan-offset)}'';
+  in {
+    enable = true;
+    # Add conntrack handling for vlan traffic
+    tables.pbr_mangle = {
+      family = "inet";
+      content = ''
+        chain prerouting {
+          type filter hook prerouting priority mangle; policy accept;
+
+          # Restore PBR mark from conntrack
+          meta mark set ct mark
+
+          # Mark previously unmarked connections according to ingress VLAN
+          ${mkVlanMark "servers"}
+          ${mkVlanMark "lan"}
+          ${mkVlanMark "iot-net"}
+
+          # Apply newly assigned connection mark to this packet
+          meta mark set ct mark
+        }
+
+        chain output {
+          type route hook output priority mangle; policy accept;
+
+          # Apply PBR mark to locally generated packets
+          meta mark set ct mark
+        }
+      '';
+    };
   };
 }
